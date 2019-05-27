f48df27844a248811714199405bbed6881c8dd95
picklecai <pickle.ahcai@163.com>
2019-05-27 13:45:49 +0800

    时钟增加暂停和继续按钮

--- _src/kivy/clock.py
+++ _src/kivy/clock.py
@@ -9,6 +9,9 @@ import time
 
 
 class MyForm(BoxLayout):
+    global switch
+    switch = 1
+
     def start(self):
         global stime, dtime
         stime = time.time()
@@ -23,6 +26,16 @@ class MyForm(BoxLayout):
             self.ids['time_counter'].text = '00:00.00'
             return False
         self.ids['time_slider'].value = dtime + stime - time.time()
+
+    def pause(self, *argv):
+        global switch
+        switch = switch * (-1)
+        if switch == -1:
+            Clock.unschedule(self.callback)
+            self.ids['bg_image'].source = 'giphy.png'
+        else:
+            Clock.schedule_interval(self.callback, 0.1)
+            self.ids['bg_image'].source = 'giphy.gif'
 
     def stop(self):
         self.ids['time_slider'].value = 0
