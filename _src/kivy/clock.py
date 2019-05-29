20f39b4c0c6b9610b6e9d893602247f80f840a57
picklecai <pickle.ahcai@163.com>
2019-05-29 16:03:18 +0800

    时钟增加声音提醒

--- _src/kivy/clock.py
+++ _src/kivy/clock.py
@@ -6,6 +6,7 @@ from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
 from kivy.clock import Clock
 import time
+import subprocess
 
 
 class MyForm(BoxLayout):
@@ -24,6 +25,8 @@ class MyForm(BoxLayout):
         if stime + dtime < time.time():
             self.ids['time_slider'].value = 0
             self.ids['time_counter'].text = '00:00.00'
+            self.ids['bg_image'].source = 'giphy.png'
+            subprocess.Popen(['open', 'alarm.wav'])
             return False
         self.ids['time_slider'].value = dtime + stime - time.time()
 
